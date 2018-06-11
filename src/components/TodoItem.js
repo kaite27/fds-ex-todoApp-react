25aba7a8bad4f98c10becb5817da8cde79b3412f
Kim Hyemin <beigenut@gmail.com>
2018-06-11 16:28:15 +0900

    Add a eventListener on TodoItemBody to popup prompt to get new sencence

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 
 export default class TodoItem extends Component {
+  handleBodyClick = e => {
+    const newBody = prompt('Enter new sentence which want to modify')
+    const {id, onBodyUpdate} = this.props
+    onBodyUpdate(id, newBody)
+  }
   render() {
     const {id, body, complete, onComplete, onDelete} = this.props
     return (
       <li className={complete ? 'complete' : ''} key={id}>
         <button onClick={e => { onComplete(id) }}>Done</button>
-        {body}
+        <span className="todoBody" onClick={this.handleBodyClick}>{ body }</span>
         <button onClick={e => { onDelete(id) }}>Delete</button>
       </li>
     )
