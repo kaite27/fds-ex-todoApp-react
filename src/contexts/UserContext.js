d2cb00b2e0a7fa9a319eebef7792004c2b27effe
Kim Hyemin <beigenut@gmail.com>
2018-06-18 12:07:08 +0900

    Add an error message when login failed

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,23 +5,30 @@ const {Provider , Consumer} = React.createContext()
 
 class UserProvider extends Component {
   login = async (username, password) => {
+    try {
     // post payload to todoAPI
-    const payload = {
-      username: username,
-      password: password
-    }
-    const res = await todoAPI.post('/users/login', payload) 
-    // Store token to localStorage 
-    localStorage.setItem('token', res.data.token)
+      const payload = {
+        username: username,
+        password: password
+      }
+      const res = await todoAPI.post('/users/login', payload) 
+      // Store token to localStorage 
+      localStorage.setItem('token', res.data.token)
 
-    // Make state empty
-    this.setState({
-      username: '',
-      password: ''
-    })
-  
-    // Move to TODO App page
-    this.props.onLogin()
+      // Make state empty
+      this.setState({
+        username: '',
+        password: ''
+      })
+    
+      // Move to TODO App page
+      this.props.onLogin()
+    } catch (e) {
+      if(e.response) {
+        if(e.response.status === 400)
+        alert('Wrong ID or Password!')
+      }
+    }
   }
 
   render() {
