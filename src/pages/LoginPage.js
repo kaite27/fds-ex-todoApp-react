154bc0987a2ac3a684bae48ea28caab858b26516
Kim Hyemin <beigenut@gmail.com>
2018-06-18 17:34:59 +0900

    Reorganize code : Leave LoginFormContain only

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
-import LoginForm from '../components/LoginForm.js'
-import {UserConsumer} from '../contexts/UserContext.js'
+import LoginFormContainer from '../containers/LoginFormContainer.js'
 
 export default class LoginPage extends Component {
   render() {
     return (
-      <UserConsumer>
-        {value => (
-          <LoginForm onLogin={value.login} />
-        )}
-      </UserConsumer>
+      <LoginFormContainer />
     )
   }
 }
