585dc993fd36fb35175917a1c21382a51b6d4d99
beigenut <beigenut@gmail.com>
2019-06-05 04:30:23 -0400

    Style LoginForm container DOM

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,46 +1,61 @@
 import React, { Component } from 'react';
+import { directive } from '@babel/types';
 
 export default class LoginForm extends Component {
-  // state = {
-  //   username: '',
-  //   password: ''
-  // }
+  state = {
+    username: '',
+    password: ''
+  }
 
-  // handleInputChangeID = (e) => {
-  //   this.setState({
-  //     username: e.target.value
-  //   })
-  // }
+  handleInputChangeID = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
 
-  // handleInputChangePW = (e) => {
-  //   this.setState({
-  //     password: e.target.value
-  //   })
-  // }
+  handleInputChangePW = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
 
   // ref 사용해서 input 제어하기
-  usernameRef = React.createRef()
-  passwordRef = React.createRef()
+  // usernameRef = React.createRef()
+  // passwordRef = React.createRef()
   
   handleSubmit = e => {
     e.preventDefault()
-    // 중간 평가때랑 똑같이 하기 위해서는 아래 input name="" 추가
-    // Change into onlogin(username, password) 
-    // const username = e.target.elements.username.value
-    // const password = e.target.elements.password.value
+    const username = this.state.username
+    const password = this.state.password
     
     const {onLogin} = this.props
-    onLogin(this.usernameRef.current.value, this.passwordRef.current.value)
+    // onLogin(this.usernameRef.current.value, this.passwordRef.current.value)
+    onLogin(username, password)
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>LOGIN PAGE</h1>        
-        <input ref={this.usernameRef} type="text" placeholder="Your ID"/>
-        <input type="password" ref={this.passwordRef} placeholder="Your Password"/>
-        <button>Login</button>
-      </form>
+      <section className="LoginForm">
+        <div className="design-box">
+          <h2>LOGIN</h2>
+          <p>ID: admin PW: 0 - test purpose only</p>
+          <form onSubmit={this.handleSubmit}>
+            <div className="inputBox">
+              {/* <input ref={this.usernameRef} type="text" /> */}
+              <input onChange={this.handleInputChangeID} value={this.state.username} type="text" />
+              <label className={this.state.username?'valid':''}>Username</label>
+            </div>
+            <div className="inputBox">
+              <input type="password" onChange={this.handleInputChangePW} value={this.state.password} />
+              <label className={this.state.password?'valid':''}>Password</label>
+            </div>
+            <div className="buttonBox">
+              <button>Login</button>
+              <button type="button" disabled>Sign in</button>
+            </div>
+          </form>
+        </div>
+      </section>
     )
   }
 }
